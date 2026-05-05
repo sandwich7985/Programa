0f1a7ab8368847b786c4b895ee12215bb8827c5c
sandwich7985 <sandwich7985@gmail.com>
2026-05-05 18:24:57 -0500

    Update Diccionario.py

--- Diccionario.py
+++ Diccionario.py
@@ -1,14 +1,49 @@
-meme_dict = {
-            "CRINGE": "Algo excepcionalmente raro o embarazoso",
-            "LOL": "Una respuesta común a algo gracioso",
-            }
+import discord
+import math
+import random
+from discord.ext import commands
 
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+#permisos
+intents = discord.Intents.default()
+intents.message_content = True
 
-if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
-    print(meme_dict[word])
-    
-else:
-    # ¿Qué hacer si no se encuentra la palabra?
-    print('La palabra no fue encontrada en el diccionario')
+#prefijo
+bot = commands.Bot(command_prefix = "/" , intents = intents)
+
+@bot.command(name = "saluda")
+async def saludar(ctx):
+    await ctx.send("Bienvenido!")
+
+
+@bot.command(name = "suma")
+async def suma(ctx, n1: int , n2: int): 
+    resultado = n1 + n2
+    await ctx.send(f"Resultado = {resultado}")
+
+@bot.command(name = "potencia")
+async def potencia(ctx, n1: int , n2: int): 
+    resultado = n1 ** n2
+    await ctx.send(f"Resultado = {resultado}")
+
+@bot.command(name = "raiz")
+async def raiz(ctx, n1: int): 
+    resultado = math.sqrt(n1)
+    await ctx.send(f"Resultado = {resultado}")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command(name = "despedir")
+async def despedir(ctx):
+    await ctx.send("Hasta luego!")
+
+bot.run("")
